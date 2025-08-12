ef5f1df911a5e96a75b808a0ea8bcc466e9864d9
Jose Manuel Arce <91504311+ManuelArce2@users.noreply.github.com>
2025-08-11 18:37:12 -0600

    funcion agregar

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//crear array con los nombres de los amigos
+let listaAmigos = []
+// funcion agregaramigo y verificar que no se repita
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (amigo === "") {
+        alert("Por favor, ingresa un nombre");
+        return;
+    }
+    if (listaAmigos.includes(amigo)) {
+        alert("Por favor, ingresa un nombre diferente");
+        return;
+    }
+    listaAmigos.push(amigo);
+    document.getElementById("listaAmigos").innerHTML = listaAmigos.join("<br>");
+    document.getElementById("amigo").value = "";
+    document.getElementById("button-add").style.backgroundColor = "#fe652b";
+}       
+ 
+//funcion para sortear amigo secreto sin repetir nombre 
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("Por favor, ingresa un nombre");
+        return;
+    }
+    let amigo = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+    document.getElementById("resultado").innerHTML = amigo;
+}   
